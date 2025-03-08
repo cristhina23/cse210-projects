48037bb0846969a21d4c2a4f988f759dd458e2a8
Cristhina <viszellechacon@gmail.com>
2025-03-08 00:46:50 -0500

    all done

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,72 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Exercise 4.");
+
+
+        List<int> numbers = new List<int>();
+        int number;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        Console.Write("Enter a number to the list: ");
+        number = int.Parse(Console.ReadLine());
+        
+
+        if (number == 0) {
+            Console.WriteLine("The list is empty.");
+            return;
+        } else {
+            numbers.Add(number);
+        }
+
+        while (number != 0) {
+
+
+            Console.Write("Enter a number to the list: ");
+            number = int.Parse(Console.ReadLine());
+            if (number == 0) {
+                break;
+            }
+            numbers.Add(number);
+        }
+
+        Console.Write("The sum of the list is: ");
+        int total = 0;
+        foreach (int num in numbers) {
+            total += num;
+        }
+        Console.WriteLine(total);
+
+        Console.Write("The average of the list is: ");
+        double average = (double)total / numbers.Count;
+        Console.WriteLine(average);
+
+        Console.Write("The largest number in the list is: ");
+        int largest = numbers[0];
+        foreach (int num in numbers) {
+            if (num > largest) {
+                largest = num;
+            }
+        }
+        Console.WriteLine(largest);
+
+        Console.Write("The smallest number in the list is: ");
+        int smallest = numbers[0];
+        foreach (int num in numbers) {
+            if (num < smallest) {
+                smallest = num;
+            }
+        }
+        Console.WriteLine(smallest);
+
+
+        
+
+
     }
 }
