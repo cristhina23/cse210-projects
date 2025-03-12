fbdafa1c1c7f6ab684a0641d37c22323726d42c6
Cristhina <viszellechacon@gmail.com>
2025-03-11 21:03:49 -0500

    resume activity

---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic; 
+
+public class Resume
+{
+  public string _Name = "";
+  public List<Job> _Jobs = new List<Job>();
+
+  public void Display() 
+  {
+    Console.WriteLine($"Name: {_Name}");
+    Console.WriteLine("Jobs:");
+    foreach (Job job in _Jobs) {
+      job.Display();
+    }
+  }
+}
