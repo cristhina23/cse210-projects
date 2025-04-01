07bf69e0458a68fdc03f09c531288e614a72563d
Cristhina <viszellechacon@gmail.com>
2025-03-31 20:36:36 -0500

    week05

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment assignment1 = new Assignment("Carlos", "test 01");
+
+        Console.WriteLine(assignment1.GetSumary());
+
+        MathAssignment mathAssignment1 = new MathAssignment("Cristhina Chacon", "Multiplication", "23", "12-25");
+
+        Console.WriteLine(mathAssignment1.GetSumary());
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+        
+        WritingAssignment writingAssignment1 = new WritingAssignment("Cristhina Cahcon", "Religion", "The book of Mormon");
+        Console.WriteLine(writingAssignment1.GetSumary());
+        Console.WriteLine(writingAssignment1.GetWritingInformation());
+
     }
 }
