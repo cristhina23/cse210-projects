79859a3abb2e02ed27c2cdde9273ffe24da60812
Cristhina <viszellechacon@gmail.com>
2025-04-13 19:59:29 -0500

    week07

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        Console.WriteLine("Hello World! This is the ExerciseTracking Project. 😎");
 
         List<Activity> activities = new List<Activity>();
 
